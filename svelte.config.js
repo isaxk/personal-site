30a82f79e513e30d5d4a0869cf270a84afb8dbfb
Isaac Boorman <isaacb@MacBookAir.vodafone.ultrahub>
2025-08-18 17:33:34 +0100

    vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,25 +1,16 @@
 import { mdsvex } from 'mdsvex';
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('mdsvex').MdsvexOptions} */
-const mdsvexOptions = {
-	extensions: ['.md']
-};
+const mdsvexOptions = { extensions: ['.md'] };
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: [vitePreprocess(), mdsvex(mdsvexOptions)],
-
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	},
-
+	kit: { adapter: adapter() },
 	extensions: ['.svelte', '.svx', '.md']
 };
 
